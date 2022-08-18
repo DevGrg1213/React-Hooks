271315da8f721a984aaf308445bbc4c43f72af2f
Dev Gurung <theyev1213@gmail.com>
2022-08-18 15:44:00 +0545

    finished useState hooks

--- use-state/src/App.js
+++ use-state/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [color, setColor] = useState("blue");
+  function increment() {
+    setCount((prevCount) => prevCount + 1);
+    setColor("green");
+  }
+  function decrement() {
+    setCount((prevCount) => prevCount - 1);
+    setColor("red");
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={increment}>+</button>
+      <h2
+        style={{
+          backgroundColor: color,
+          display: "inline-block",
+          marginInline: "10px",
+          padding: "10px",
+        }}
+      >
+        {count}
+      </h2>
+      <button onClick={decrement}>-</button>
     </div>
   );
 }
